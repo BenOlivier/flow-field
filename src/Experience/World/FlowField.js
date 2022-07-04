795929aa3b4174679b137a17f533f8aa60ea47e0
BenOlivier <dbeolivier@gmail.com>
2022-07-04 01:35:20 +0100

    Implement 2D grid

--- src/Experience/World/FlowField.js
+++ src/Experience/World/FlowField.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { Quaternion } from 'three'
 import Experience from '../Experience.js'
 
 export default class Object
@@ -17,21 +18,25 @@ export default class Object
     setGrid()
     {
         const steps = 10
+        const spacing = 1
         const points = []
 
-        const cubeGeo = new THREE.BoxGeometry(0.1, 0.1, 0.1)
+        const cubeGeo = new THREE.BoxGeometry(0.05, 0.3, 0.05)
         const cubeMat = new THREE.MeshBasicMaterial({color: '#ffffff'})
-        const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat)
 
-        for(let i = 0; i < steps; i++)
+        for(let column = 0; column < steps; column++)
         {
-            for(let j = 0; j < steps; j++)
+            for(let row = 0; row < steps; row++)
             {
-                const point = {x: i - 4.5, y: j - 4.5}
+                const xPos = spacing * column - (steps / 2 * spacing) + (spacing / 2)
+                const yPos = spacing * row - (steps / 2 * spacing) + (spacing / 2)
+                const zRot = column / steps * Math.PI
+                const point = {x: 0, y: 0, z: zRot}
                 points.push(point)
 
                 const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat)
-                cubeMesh.position.set(point.x, point.y, 0)
+                cubeMesh.position.set(xPos, yPos, 0)
+                cubeMesh.quaternion.setFromEuler(new THREE.Euler(point.x, point.y, point.z))
                 this.scene.add(cubeMesh)
             }
         }
