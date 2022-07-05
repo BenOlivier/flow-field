12177e032186d60a273c94b7430be7339555bbf9
BenOlivier <dbeolivier@gmail.com>
2022-07-05 22:30:03 +0100

    Somewhat tamed field

--- src/Experience/World/FlowField.js
+++ src/Experience/World/FlowField.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import Experience from '../Experience.js'
 import PerlinNoise from '../Utils/PerlinNoise.js'
+import Noise from '../Utils/Noise.js'
+import SimplexNoise from 'simplex-noise'
 // import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline'
 
 export default class flowField
@@ -9,15 +11,17 @@ export default class flowField
     {
         this.experience = new Experience()
         this.perlinNoise = new PerlinNoise()
+        this.noise = new Noise()
         this.scene = this.experience.scene
         this.sizes = this.experience.sizes
         this.time = this.experience.time
         this.debug = this.experience.debug
 
         this.params = {
-            gridSize: 5,
-            gridStep: 1,
-            noiseScale: 0.1
+            linesCount: 10,
+            sphereRadius: 5,
+            noiseScale: 0.001,
+            pointFrequency: 0.1
         }
 
         if(this.debug.active)
@@ -37,13 +41,18 @@ export default class flowField
             this.debugFolder.addColor(this.params, 'particleColor')
         }
 
+        const simplex = new SimplexNoise()
+        // value2d = simplex.noise2D(x, y),
+        // value3d = simplex.noise3D(x, y, z),
+        // value4d = simplex.noise4D(x, y, z, w);
+
         this.lines = []
         this.flowField = new THREE.Object3D()
         this.scene.add(this.flowField)
 
-        function CreateLine(x, y, z){
+        function CreateLine(position){
             const line = {
-                points: [new THREE.Vector3(x, y, z)],
+                points: [position],
                 angle: new THREE.Euler(0, 0, 0),
                 vec3: new THREE.Vector3(0, 0, 0),
                 length: 10
@@ -52,13 +61,18 @@ export default class flowField
         }
 
         // Instantiate lines
-        for(let i = -0.5 * this.params.gridSize; i <= 0.5 * this.params.gridSize; i += this.params.gridStep)
-        {
-            for(let j = -0.5 * this.params.gridSize; j <= 0.5 * this.params.gridSize; j += this.params.gridStep)
-            {
-                for(let k = -0.5 * this.params.gridSize; k <= 0.5 * this.params.gridSize; k += this.params.gridStep)
-                {
-                    const line = CreateLine(i, j, k)
+        // for(let i = 0; i < this.params.linesCount; i++)
+        // {
+        //     const direction = new THREE.Vector3(
+        //         Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize()
+        //     const position = direction.multiplyScalar(this.params.sphereRadius)
+        //     const line = CreateLine(position)
+        //     this.lines.push(line)
+        // }
+        for (let z = -1; z < 1; z += 0.2) {
+            for (let y = -1; y < 1; y += 0.2) {
+                for (let x = -1; x < 1; x += 0.2) {
+                    const line = CreateLine(new THREE.Vector3(x, y, z))
                     this.lines.push(line)
                 }
             }
@@ -69,16 +83,16 @@ export default class flowField
             const positions = []
             for(let i = 0; i < line.length; i ++)
             {
-                const noise = this.perlinNoise.noise(
+                const noise = simplex.noise3D(
                     line.points[i].x * this.params.noiseScale,
                     line.points[i].y * this.params.noiseScale,
                     line.points[i].z * this.params.noiseScale
-                ) * Math.PI * 4
+                ) * Math.PI * 16
 
-                line.angle.set(noise, noise, noise)
-                line.vec3.set(1, 1, 1)
+                line.angle.set(Math.cos(noise), Math.sin(noise), Math.cos(noise))
+                line.vec3.set(line.points[i].x, line.points[i].y, line.points[i].z)
                 line.vec3.applyEuler(line.angle)
-                line.vec3.multiplyScalar(0.1)
+                line.vec3.multiplyScalar(this.params.pointFrequency)
                 line.points.push(new THREE.Vector3(
                     line.points[i].x + line.vec3.x,
                     line.points[i].y + line.vec3.y,
