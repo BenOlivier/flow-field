9b3077c81dd1c780efc174f3a48b18e1d2494092
BenOlivier <dbeolivier@gmail.com>
2022-07-16 12:58:03 +0100

    Refactor sketch

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,5 @@
-// Based on vector-field by Varun Vachhar
-// https://github.com/winkerVSbecks/sketchbook/blob/master/colour-field.js
+// Based on flow-field by Varun Vachhar
+// https://github.com/winkerVSbecks/sketchbook/blob/master/flow-field.js
 
 import './style.css';
 
@@ -10,10 +10,15 @@ const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
 const { mapRange } = require('canvas-sketch-util/math');
 const lerp = require('lerp');
 const simplex = new SimplexNoise();
+const poisson = new FastPoissonDiskSampling({
+    shape: [window.innerWidth, window.innerHeight],
+    radius: 20,
+    tries: 20,
+});
 
 const settings = {
     // animate: true,
-    // duration: 10,
+    duration: 5,
 };
 
 const colors = {
@@ -23,72 +28,76 @@ const colors = {
     white: '#ffffff',
 };
 
+const debug = true;
 const damping = 0.1;
 const step = 10;
 const particleSteps = 60;
 
-canvasSketch(() =>
+const sketch = () =>
 {
-    return ({ context, width, height, playhead }) =>
-    {
-        context.clearRect(0, 0, width, height);
-        context.fillStyle = colors.black;
-        context.fillRect(0, 0, width, height);
+    let particles = [];
+    let stepsTaken = 0;
 
-        const padding = 100;
-        const gridDensity = 40;
-        const gridSize = [width / gridDensity, height / gridDensity];
-        const tileSize = (width - padding * 2) / gridSize[0];
-        const vectorLength = tileSize * 0.5;
-        const vectorThickness = 2;
-        const time = playhead;
-
-        for (let x = 0; x < gridSize[0]; x++)
+    return {
+        begin()
         {
-            for (let y = 0; y < gridSize[1]; y++)
+            // Generate evenly distributed starting points
+            particles = poisson.fill();
+        },
+        render({ context, width, height, playhead })
+        {
+            context.clearRect(0, 0, width, height);
+            context.fillStyle = colors.black;
+            context.fillRect(0, 0, width, height);
+
+            if (debug)
             {
-                // get a 0-1 UV coordinate
-                const u = x / (gridSize[0] - 1);
-                const v = y / (gridSize[1] - 1);
-
-                // scale to dimensions with a border padding
-                const uv = {
-                    x: lerp(padding, width - padding, u),
-                    y: lerp(padding, height - padding, v),
-                };
-
-                // Draw
-                context.save();
-                context.fillStyle = colors.white;
-
-                const rotation = simplex.noise3D(x / gridSize[0],
-                    y / gridSize[1], time) * Math.PI;
-
-                // Rotate in place
-                context.translate(uv.x, uv.y);
-                context.rotate(rotation);
-                context.translate(-uv.x, -uv.y);
-
-                // Draw the line
-                context.fillRect(uv.x, uv.y - vectorThickness, vectorLength, vectorThickness);
-                context.restore();
+                drawVectorField(context, width, height);
             }
-        }
+        },
+    };
+};
 
-        // Generate evenly distributed starting points
-        const poisson = new FastPoissonDiskSampling({
-            shape: [width, height],
-            radius: 20,
-            tries: 20,
-        });
-        const points = poisson.fill();
+canvasSketch(sketch, settings);
 
-        for (const point of points)
+function drawVectorField(context, width, height)
+{
+    const padding = 100;
+    const gridDensity = 40;
+    const gridSize = [width / gridDensity, height / gridDensity];
+    const tileSize = (width - padding * 2) / gridSize[0];
+    const vectorLength = tileSize * 0.5;
+    const vectorThickness = 2;
+
+    for (let x = 0; x < gridSize[0]; x++)
+    {
+        for (let y = 0; y < gridSize[1]; y++)
         {
+            // get a 0-1 UV coordinate
+            const u = x / (gridSize[0] - 1);
+            const v = y / (gridSize[1] - 1);
+
+            // scale to dimensions with a border padding
+            const uv = {
+                x: lerp(padding, width - padding, u),
+                y: lerp(padding, height - padding, v),
+            };
+
+            // Draw
             context.save();
-            context.fillStyle = colors.blue;
-            context.fillRect(point[0], point[1], 4, 4);
+            context.fillStyle = colors.white;
+
+            const rotation = simplex.noise3D(x / gridSize[0],
+                y / gridSize[1], 0) * Math.PI;
+
+            // Rotate in place
+            context.translate(uv.x, uv.y);
+            context.rotate(rotation);
+            context.translate(-uv.x, -uv.y);
+
+            // Draw the line
+            context.fillRect(uv.x, uv.y - vectorThickness, vectorLength, vectorThickness);
             context.restore();
         }
-    };
-}, settings);
+    }
+}
