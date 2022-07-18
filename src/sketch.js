bbfff4c0c864c17a05198e5b146d869534026322
BenOlivier <dbeolivier@gmail.com>
2022-07-18 10:09:03 +0100

    Add hidden canvas

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,12 @@
 // Based on flow-field by Varun Vachhar
 // https://github.com/winkerVSbecks/sketchbook/blob/master/flow-field.js
 
-import { LineLoop } from 'three';
 import './style.css';
 
 const canvasSketch = require('canvas-sketch');
 const SimplexNoise = require('simplex-noise');
 const FastPoissonDiskSampling = require('fast-2d-poisson-disk-sampling');
 const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
-// const { mapRange } = require('canvas-sketch-util/math');
-// const lerp = require('lerp');
 const simplex = new SimplexNoise();
 const poisson = new FastPoissonDiskSampling({
     shape: [window.innerWidth, window.innerHeight],
@@ -17,16 +14,27 @@ const poisson = new FastPoissonDiskSampling({
     tries: 20,
 });
 
-// class Rectangle
-// {
-//     constructor(x, y, w, h)
-//     {
-//         this.x = x;
-//         this.y = y;
-//         this.w = w;
-//         this.h = h;
-//     }
-// }
+const hiddenCanvas = document.createElement('canvas');
+Object.assign(hiddenCanvas.style, {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+});
+document.body.appendChild(hiddenCanvas);
+const hiddenContext = hiddenCanvas.getContext('2d');
+
+// Lookup the size the browser is displaying the canvas in CSS pixels. //TODO: Do on resize
+const displayWidth = hiddenCanvas.clientWidth;
+const displayHeight = hiddenCanvas.clientHeight;
+// Check if the canvas is not the same size.
+const needResize = hiddenCanvas.width !== displayWidth ||
+    hiddenCanvas.height !== displayHeight;
+if (needResize)
+{
+    // Make the canvas the same size
+    hiddenCanvas.width = displayWidth;
+    hiddenCanvas.height = displayHeight;
+}
 
 const colors = {
     red: '#da3900',
@@ -80,6 +88,10 @@ const sketch = () =>
             context.clearRect(0, 0, width, height);
             context.fillStyle = colors.black;
             context.fillRect(0, 0, width, height);
+            // Clear hidden canvas
+            hiddenContext.clearRect(0, 0, width, height);
+            hiddenContext.fillStyle = '#ffffff';
+            hiddenContext.fillRect(0, 0, width, height);
 
             // If steps are not complete
             if (stepsTaken < numSteps)
@@ -91,13 +103,13 @@ const sketch = () =>
                     if (line.points.length === stepsTaken)
                     {
                         // If the current position is not to close to other points
-                        if (checkProximity(line.x, line.y, lines) === true)
+                        if (checkProximity(line.x, line.y) === true)
                         {
                             extendLine(line, width, height);
                         }
                         else
                         {
-                            // console.log('too close!');
+                            console.log('too close!');
                         }
                     }
                     if (line.points.length === 0) // TODO: Delete
@@ -132,9 +144,21 @@ const sketch = () =>
                 {
                     context.lineTo(...pt);
                 });
-
                 context.strokeStyle = lines[index].color;
                 context.stroke();
+
+                // Thick lines
+                hiddenContext.beginPath();
+                hiddenContext.moveTo(...start);
+                pts.forEach((pt) =>
+                {
+                    hiddenContext.lineTo(...pt);
+                });
+                hiddenContext.lineWidth = 50;
+                hiddenContext.strokeStyle = '#000000';
+                hiddenContext.lineJoin = 'round';
+                hiddenContext.lineCap = 'round';
+                hiddenContext.stroke();
             });
             stepsTaken++;
         },
@@ -143,28 +167,15 @@ const sketch = () =>
 
 canvasSketch(sketch, settings);
 
-function checkProximity(x, y, lines)
+function checkProximity(x, y)
 {
-    // For each point in the vicinity
-    // lines.forEach((p) =>
-    // {
-    //     // If distance between [x,y] and point > minDistance return true
-    // });
-    const pointX = window.innerWidth / 2;
-    const pointY = window.innerHeight / 2;
+    const pixelData = hiddenContext.getImageData(x, y, 1, 1).data;
+    console.log(pixelData);
 
-    const distance = getDistance(x, y, pointX, pointY);
-    if (distance > minDistance)
+    if (pixelData[0] === 255)
     {
         return true;
     }
-}
-
-function getDistance(x1, y1, x2, y2)
-{
-    const x = x2 - x1;
-    const y = y2 - y1;
-    return Math.sqrt(x * x + y * y);
 }
 
 function extendLine(line, width, height)
